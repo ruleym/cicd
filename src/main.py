901f1c60974d7b2d1f59898052b21bb62ef7bf2e
ruleym <ruleym@my.erau.edu>
2023-04-25 22:47:50 -0700

    Updating how the function

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 """
-odd/even number checker
-Author: Wolf Paulus (https://wolfpaulus.com)
+Perfect square checker
+Author: Molly Ruley
 """
 
 
-def is_odd(num: int) -> bool:
-    """Return True if num is odd, False otherwise."""
-    return num % 2 == 1
+def is_square(num: int) -> bool:
+    """Return True if num is a perfect square, False otherwise."""
+    return (num ** .5).is_integer()
 
 
-def is_odd_str(num: str) -> str:
-    """Return a string indicating whether num is odd or even."""
+def is_square_str(num: str) -> str:
+    """Return a string indicating whether num is a perfect square or not."""
     if num.isnumeric():
-        return f"{num} is {'odd' if is_odd(int(num)) else 'even'}."
+        return f"{num} is {'a perfect square' if is_square(int(num)) else 'not a perfect square'}."
     else:
         return "Please enter a number."
+
