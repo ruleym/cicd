033217765785faadf1ad8a80a98f1695698664c1
ruleym <ruleym@my.erau.edu>
2023-04-25 22:48:23 -0700

    Update to the test files

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,19 +3,19 @@ Test the main module.
 Author: Wolf Paulus (wolf@paulus.com)
 """
 from unittest import TestCase
-from main import is_odd, is_odd_str
+from main import is_square, is_square_str
 
 
 class Test(TestCase):
-    def test_is_odd(self):
-        assert not is_odd(0)
-        assert is_odd(1)
-        assert not is_odd(2)
+    def test_is_square(self):
+        assert not is_square(14)
+        assert is_square(9)
+        assert not is_square(8)
 
-    def test_is_odd_str(self):
-        assert is_odd_str("0") == "0 is even."
-        assert is_odd_str("1") == "1 is odd."
-        assert is_odd_str("2") == "2 is even."
-        assert is_odd_str("-1") == "Please enter a number."
-        assert is_odd_str("A") == "Please enter a number."
-        assert is_odd_str("") == "Please enter a number."
+    def test_is_square_str(self):
+        assert is_square_str("0") == "0 is even."
+        assert is_square_str("1") == "1 is odd."
+        assert is_square_str("2") == "2 is even."
+        assert is_square_str("-1") == "Please enter a number."
+        assert is_square_str("A") == "Please enter a number."
+        assert is_square_str("") == "Please enter a number."
